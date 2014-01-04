ff53f19a6dea12b4240ee247e8209e2ca8e85c0d
Jr Mime <vinc.bel@hotmail.com>
2014-01-03 21:59:28 -0500

    Can be the same

--- FileUsageAuto-update/newqueue.js
+++ FileUsageAuto-update/newqueue.js
@@ -5,7 +5,7 @@ if (document.getElementsByClassName("oldName") || document.getElementsByClassNam
 }
 
 function fuauModal() {
-    $.showCustomModal("Queue addition", '<form class="WikiaForm" method="" name=""><fieldset><strong>Old file name: </strong><br /><input type="text" id="oldName-enter" placeholder="File:TSAtBarnS3 E5.png" style="width: 500px"></input><br /><strong>New file name: </strong><br /><input type="text" id="newName-enter" placeholder="File:Twilight Sparkle at the barn S3E5.png" style="width: 500px;"></input></feildset></form>', {
+    $.showCustomModal("Queue addition", '<form class="WikiaForm" method="" name=""><fieldset><strong>Old file name: </strong><br /><input type="text" id="oldName" placeholder="File:TSAtBarnS3 E5.png" style="width: 500px"></input><br /><strong>New file name: </strong><br /><input type="text" id="newName" placeholder="File:Twilight Sparkle at the barn S3E5.png" style="width: 500px;"></input></feildset></form>', {
         id: "fuau",
         width: 650,
         buttons: [{
@@ -16,7 +16,10 @@ function fuauModal() {
             id: "submit",
             defaultButton: true,
             message: "Add to queue",
-            handler: function() {addtoqueue} //Yeah, need to fill this in! :p
+            handler: addtoqueue;
         }]
     }
 }
+
+function addtoqueue()
+// oldName - newName for the button and the modal
