487d0c8a42296eeb966130473d63edd41d0d50c5
Jr Mime <vinc.bel@hotmail.com>
2014-01-06 15:33:57 -0500

    Update newqueue.js

--- FileUsageAuto-update/newqueue.js
+++ FileUsageAuto-update/newqueue.js
@@ -26,7 +26,7 @@ function fuauModal() {
 function addtoqueue() {
     var oldImageName = document.getElementsByClassName("oldName").val(),
         newImageName = document.getElementsByClassName("newName").val(),
-        reason = text("Using the queue button").val(); //Make a specific reason for this
+        reason = text.("Using the queue button").val(); //Make a specific reason for this
     LIR.pageKey = [];
 processQueue()
 }
