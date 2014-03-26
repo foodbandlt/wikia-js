5ff09261881e9ac70406887fd10931c3f13e97e9
Food <foodbandlts@gmail.com>
2014-03-25 20:53:32 -0400

    Commiting bugtesting and results

---
+++ FileUsageAuto-update/BUG TESTING/File move test THROTTLED.js
@@ -0,0 +1,73 @@
+array1 = [
+	"File1.png",
+	"File2.png",
+	"File3.png",
+	"File4.png",
+	"File5.png",
+	"File6.png",
+	"File7.png",
+	"File8.png",
+	"File9.png",
+	"File10.png"
+]
+
+array2 = [
+	"File1Test.png",
+	"File2Test.png",
+	"File3Test.png",
+	"File4Test.png",
+	"File5Test.png",
+	"File6Test.png",
+	"File7Test.png",
+	"File8Test.png",
+	"File9Test.png",
+	"File10Test.png"
+]
+
+/*
+@Param Beginning name
+@Param Ending name
+@Param Index of array
+@param Callback
+*/
+
+function moveFile(arrayOne, arrayTwo, index, callback){
+	$.ajax({
+		url: "/api.php",
+		type: "POST",
+		async: true,
+		data: {
+			action: "move",
+			from: "File:" + arrayOne[index],
+			to: "File:" + arrayTwo[index],
+			reason: "Testing file moving",
+			movetalk: true,
+			noredirect: true,
+			ignorewarnings: true,
+			token: mediaWiki.user.tokens.get("editToken"),
+			format: "json"
+		},
+		contentType: "application/x-www-form-urlencoded",
+		success: function(result){
+			if (typeof result.error === "undefined"){
+				if (console) console.log("Moved file \"" + arrayOne[index] + "\" to \"" + arrayTwo[index] + "\"");
+			}else{
+				if (console) console.log("Unable to move file \"" + arrayOne[index] + "\" to \"" + arrayTwo[index] + "\" for reason: \"" + result.error.code + "\".");	
+			}
+			
+			if (typeof(callback) === "function" && (index+1) != arrayOne.length){
+				callback(arrayOne, arrayTwo, index+1, callback);
+			}
+		}
+	});
+}
+
+/*****************************
+Swap param 1 and 2 as needed
+
+moveFile(array1, array2, 0, function(arrayOne, arrayTwo, index, callback){
+	moveFile(arrayOne, arrayTwo, index, callback);
+	}
+);
+
+*****************************/
--- FileUsageAuto-update/BUG TESTING/File move test UNTHROTTLED.js
+++ FileUsageAuto-update/BUG TESTING/File move test UNTHROTTLED.js
@@ -1,33 +1,35 @@
 array1 = [
-	File1.png,
-	File2.png,
-	File3.png,
-	File4.png,
-	File5.png,
-	File6.png,
-	File7.png,
-	File8.png,
-	File9.png,
-	File10.png
+	"File1.png",
+	"File2.png",
+	"File3.png",
+	"File4.png",
+	"File5.png",
+	"File6.png",
+	"File7.png",
+	"File8.png",
+	"File9.png",
+	"File10.png"
 ]
 
 array2 = [
-	File10.png,
-	File20.png,
-	File30.png,
-	File40.png,
-	File50.png,
-	File60.png,
-	File70.png,
-	File80.png,
-	File90.png,
-	File100.png
+	"File1Test.png",
+	"File2Test.png",
+	"File3Test.png",
+	"File4Test.png",
+	"File5Test.png",
+	"File6Test.png",
+	"File7Test.png",
+	"File8Test.png",
+	"File9Test.png",
+	"File10Test.png"
 ]
 
+/*
+@Param Beginning name
+@Param Ending name
+*/
 
-
-
-moveFiles: function(arrayOne, arrayTwo, callback) {
+function moveFiles(arrayOne, arrayTwo){
 	/* Arrays are assumed to be the same length */
 	for (var index = 0; index < arrayOne.length; index++){
 		$.ajax({
@@ -49,17 +51,8 @@ moveFiles: function(arrayOne, arrayTwo, callback) {
 			success: function(result){
 				if (typeof result.error === "undefined"){
 					if (console) console.log("Moved file \"" + arrayOne[index] + "\" to \"" + arrayTwo[index] + "\"");
-
-					/* Call callback if exists */
-					if (typeof(callback) === "function" && index++ == arrayOne.length){
-						callback(index);
-					}
 				}else{
-					if (console) console.log("Unable to move file \"" + arrayOne[index] + "\" to \"" + arrayTwo[index] + "\" for reason: " + result.error.code + ".");
-					
-					if (typeof(callback) === "function" && index++ == arrayOne.length){
-						callback(index);
-					}
+					if (console) console.log("Unable to move file \"" + arrayOne[index] + "\" to \"" + arrayTwo[index] + "\" for reason: \"" + result.error.code + "\".");
 				}
 			}
 		});
